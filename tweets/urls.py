9562967b4eabb5c0b2bf74f4ab2a4a51311cfd57
Will C <9823054+WillamysC@users.noreply.github.com>
2022-08-04 23:02:07 -0300

    tweet create form added.

--- tweets/urls.py
+++ tweets/urls.py
@@ -5,6 +5,7 @@ app_name = "tweets"
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('create-tweet', views.tweet_create_view, name='create-tweet'),
     path('tweets', views.tweet_list_view, name='tweets-list'),
     path('tweets/<int:tweet_id>', views.tweet_detail_view, name='tweet-details'),
 ]
