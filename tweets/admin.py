2a1265ce94e6f7597d57aa015ba52de32789a2b7
Willamys <98623054+WillamysC@users.noreply.github.com>
2022-08-18 09:44:38 -0300

    Like field added

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,15 +1,18 @@
-from re import T
 from django.contrib import admin
 
 # Register your models here.
-from .models import Tweet
+from .models import Tweet, TweetLike
+
+class TweetLikeAdmin(admin.TabularInline):
+    model = TweetLike
 
 
 class TweetAdmin(admin.ModelAdmin):
+    inlines = [TweetLikeAdmin]
     list_display = ['__str__', 'user']
     search_fields = ['content','user__username', 'user__email']
     class Meta:
         model = Tweet
 
 
-admin.site.register(Tweet, TweetAdmin)
+admin.site.register(Tweet, TweetAdmin) 
